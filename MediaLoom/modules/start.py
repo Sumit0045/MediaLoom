706a7cf6945bcf4d7cd25bbcbac59333e5ddec36
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-04-29 11:53:46 +0530

    Update start.py

--- MediaLoom/modules/start.py
+++ MediaLoom/modules/start.py
@@ -12,8 +12,7 @@ buttons = InlineKeyboardMarkup([
 
 @app.on_message(filters.command("start"))
 async def start_(_, message):
-    await message.reply_photo(photo="https://graph.org/file/371c915b8c97ecf62a2d6-edbae9f1a9a3ff4add.jpg",
-        caption=f"Hello, I'm {BOT_NAME}! I can host any type of media, including large files, with unlimited storage. "
-        "Feel free to upload your files, and I'll take care of the rest!",
+  # if you want add photo replace this url  await message.reply_photo(photo="https://graph.org/file/371c915b8c97ecf62a2d6-edbae9f1a9a3ff4add.jpg",
+    await message.reply_text(f"Hello, I'm {BOT_NAME}! I can host any type of media, including large files, with unlimited storage.Feel free to upload your files, and I'll take care of the rest!",
         reply_markup=buttons
     )
